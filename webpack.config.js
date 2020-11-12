9caead70c38662691f74eee1a1730292ca87369d
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-11-12 11:26:46 -0500

    add linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js",
+    filename: 'app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
+      template: 'src/index.html'
+    })
   ],
   devServer: {
-    open: true,
+    open: true
   },
   module: {
     rules: [
@@ -18,12 +18,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-};
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
