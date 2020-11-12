08633c015b123810759211bdbf2cfefeb36f0b6e
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-11-12 18:49:20 -0500

    add images library

---
+++ public/assets/images/images.js
@@ -0,0 +1,15 @@
+const images = {
+  bell: require('./bell.svg'),
+  cog: require('./cog.svg'),
+  comments: require('./comments.svg'),
+  eye: require('./eye.svg'),
+  home: require('./home.svg'),
+  search: require('./search.svg'),
+  star: require('./star.svg'),
+  news: require('./news.svg'),
+  logo: require('./logo.png'),
+  bacterium: require('./bacterium.jpg')
+
+}
+
+export default images
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,25 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
       }
     ]
   }
